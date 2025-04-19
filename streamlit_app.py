242181765ba167a260b98498565b329ac7362cfa
el-ecle <el.eclectico@gmail.com>
2025-04-19 10:57:25 -0400

    Update streamlit_app.py
    correct dataframe misspell

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.stop()
 
 # Convert the Snowpark Dataframe to a Pandas Dataframe s w can use the LOC function
-pd_df=my_dataframe)pd_df)
+pd_df=my_dataframe.to_pandas()
+sts,daaframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
